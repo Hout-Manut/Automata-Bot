f8f89c4b33908980341c4256edca682b2b834025
Hout Manut <huotmanut00@gmail.com>
2024-06-14 15:02:13 +0700

    Menu screen

--- bot/automata/screen.py
+++ bot/automata/screen.py
@@ -12,8 +12,8 @@ class MainScreen(menu.Screen):
 
     def __init__(
         self,
-        fa: FA | None = None
-        menu: menu.Menu
+        menu: menu.Menu,
+        fa: FA | None = None,
     ) -> None:
         super().__init__(menu)
 
@@ -22,4 +22,40 @@ class MainScreen(menu.Screen):
 
         )
 
-    def get_fa_embed
+    def get_fa_embed(
+        self,
+        image_ratio: str = "1",
+        author_name: str | None = None,
+        author_icon: hikari.Resourceish | None = None
+    ) -> hikari.Embed:
+        title = "Deterministic" if self.fa.is_dfa else "Non-deterministic"
+        title += " Finite Automation"
+        embed = hikari.Embed(
+            title=title,
+            description=None,
+            color=Color.LIGHT_BLUE
+        )
+        fa = self.fa
+
+        name = "State" if len(fa.states) == 1 else "States"
+        embed.add_field(name, fa.states_str)
+
+        name = "Alphabet" if len(fa.alphabets) == 1 else "Alphabets"
+        embed.add_field(name, fa.alphabets_str)
+
+        embed.add_field("Initial State", fa.initial_state)
+
+        name = "State" if len(fa.final_states) == 1 else "States"
+        embed.add_field(f"Final {name}", fa.final_states_str)
+
+        name = "Function" if len(fa.t_func) == 1 else "Functions"
+        embed.add_field(f"Transition {name}", fa.t_func_str)
+
+        embed.set_image(fa.get_diagram(image_ratio))
+        if author_name or author_icon:
+            embed.set_author(name=author_name, icon=author_icon)
+
+        return embed
+
+    @menu.button(label="Test a String", style=hikari.ButtonStyle.SECONDARY)
+    def test_string_callback(self, ctx: miru.ViewContext, btn: miru.Button) -> None:
