02bdebde21af75448e6d42baecf1329947c41a6b
Hout-Manut <huotmanut00@gmail.com>
2024-06-17 14:48:24 +0700

    Fixed regex pattern

--- bot/automata/classes.py
+++ bot/automata/classes.py
@@ -32,7 +32,7 @@ class RegexPatterns:
     BACKSPACE = re.compile(r"-(\d+)\s*$")
 
     STATES = re.compile(r"\b\w+\b")
-    ALPHABETS = re.compile(r"\w+")
+    ALPHABETS = re.compile(r"\w?")
     INITIAL_STATE = re.compile(r"\b\w+\b")
     FINAL_STATES = re.compile(r"\b\w+\b")
     TF = re.compile(r"\b(\w+)\s*[,\s]\s*(\w+)\s*(=|>|->)\s*(\w+)\b")
